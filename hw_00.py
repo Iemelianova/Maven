ea82878a78abe5a2af74c68e614415074ee53bd6
Iemelianova <62721292+Iemelianova@users.noreply.github.com>
2020-05-12 01:53:14 +0300

    Delete hw_00.py

--- hw_00.py
+++
@@ -1,6 +0,0 @@
-name = input('What is your name? ')
-age = input('How old are you? ')
-country = input('Where are you live? ')
-print ('Hello, %s!' %name)
-print('Your age is ' + age)
-print('You live in ' + country)
